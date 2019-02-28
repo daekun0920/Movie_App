a4b39a695b78097a0461a60b65594b33056945e7
Han <gkseorjs123@naver.com>
2019-02-28 23:04:52 +0900

    Updates

---
+++ precache-manifest.036b393110f7e6cb317f7c74d8cbb4c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae58143d64a8883e5f8b",
+    "url": "/Movie_App/static/js/runtime~main.ae58143d.js"
+  },
+  {
+    "revision": "527d9eb45b8271f844bc",
+    "url": "/Movie_App/static/js/main.527d9eb4.chunk.js"
+  },
+  {
+    "revision": "da87d06f04d863a24d6d",
+    "url": "/Movie_App/static/js/1.da87d06f.chunk.js"
+  },
+  {
+    "revision": "527d9eb45b8271f844bc",
+    "url": "/Movie_App/static/css/main.935fbdf0.chunk.css"
+  },
+  {
+    "revision": "354a41b967e7b040708642154cbe1d6c",
+    "url": "/Movie_App/index.html"
+  }
+];
